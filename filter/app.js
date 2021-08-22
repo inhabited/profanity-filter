2b1e9736694c240dda07a0e48dfcabc2f8561117
inhabited <pilotxt1@gmail.com>
2021-08-22 00:28:20 -0400

    messing with JS

--- filter/app.js
+++ filter/app.js
@@ -1,9 +1,26 @@
-const spotifyToken = window.location.hash.split("=")[1].split("&")[0]
+const hash = window.location.hash
+const splitOne = hash.split("=")[1]
+const spotifyToken = splitOne.split("&")[0]
+console.log(spotifyToken);
 
 fetch("https://api.spotify.com/v1/me/playlists", {
+    headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${spotifyToken}`,
+        "Content-Type": "application/json"
+    }
+})
+    .then(function (response) {
+        console.log(response);
+        => response.json())
+    .then(data => console.log(data)).then(limit = data.limit);
+
+
+
+/*fetch(`https://api.spotify.com/v1/playlists/6ZafhngGkEnEsbq3Ugtmsv/tracks?limit=100`, {
   headers: {
     Accept: "application/json",
     Authorization: `Bearer ${spotifyToken}`,
     "Content-Type": "application/json"
   }
-}).then(response => response.json()).then(data => console.log(data));
+}).then(response => response.json()).then(data => console.log(data));*/
